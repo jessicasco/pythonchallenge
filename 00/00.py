e51a4c5ac5688cab3f60416950df5122ce10c21f
Long Qian <qianlongzju@gmail.com>
2011-11-05 09:20:22 +0800

    solution to level 00

---
+++ 00/00.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Title: warming up
+
+def main():
+    print 2**38
+
+if __name__ == '__main__':
+    main()
+    #下一关：http://www.pythonchallenge.com/pc/def/274877906944.html
